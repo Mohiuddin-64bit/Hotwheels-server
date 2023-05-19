336924d159c2996301f387f8fdae651b6cff8f13
Mohiuddin <mohiuddin.niddu@gmail.com>
2023-05-19 16:38:14 +0600

    ShopByCategory function done

--- index.js
+++ index.js
@@ -1,48 +1,61 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-
-
-
-const uri = "mongodb+srv://Hotwheels:Tew51rbYMORr5APr@cluster0.bfg1fsg.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.User_name}:${process.env.password}@cluster0.bfg1fsg.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const toyCollection = client.db("hotwheels").collection("hotwheelsCollection");
+    const toyCollection = client
+      .db("hotwheels")
+      .collection("hotwheelsCollection");
 
-    app.post('/addToy', async(req, res) => {
+    // POST method
+    app.post("/addToy", async (req, res) => {
       const body = req.body;
-      console.log(body)
+      console.log(body);
       const result = await toyCollection.insertOne(body);
-      res.send(result)
-      console.log(result)
-    })
+      res.send(result);
+      console.log(result);
+    });
 
-
-
-
-
-
+    // GET method
+    app.get("/allToy/:category", async (req, res) => {
+      console.log(req.params.category);
+      if (
+        req.params.category === "sports_car" ||
+        req.params.category === "truck" ||
+        req.params.category === "regular_car"
+      ) {
+        const result = await toyCollection
+          .find({ category: req.params.category })
+          .toArray();
+          console.log(result)
+          res.send(result);
+      } 
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -50,14 +63,13 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send("hotwheels is running")
-})
-app.get('http://localhost:5000/addToy', (req, res) => {
-  res.send("hotwheels is running")
-})
+app.get("/", (req, res) => {
+  res.send("hotwheels is running");
+});
+app.get("/addToy", (req, res) => {
+  res.send("hotwheels is running");
+});
 
 app.listen(port, () => {
-  console.log(`HotWheels server is running on port, ${port}`)
-})
+  console.log(`HotWheels server is running on port, ${port}`);
+});
